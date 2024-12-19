54b07c276d8521d941060bb76723e23eb943921f
2403246954@qq.com <2403246954@qq.com>
2024-12-19 20:41:44 +0800

    对is_prime内容进行修改

--- homework/12/16/is_prime.c
+++ homework/12/16/is_prime.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 
-void is_prime(int n)
+// void is_prime(int n)//n以内的质数
+// {
+//     int count = 0;
+//     for (int i = 2; i <= n; i++)
+//     {
+//         int flag = 1;
+//         for (int j = 2; j * j <= i; j++)
+//         {
+//             if (i % j == 0)
+//             {
+//                 flag = 0;
+//                 break;
+//             }
+//         }
+//         if (flag == 1)
+//         {
+//             printf("%d\t", i);
+//             count++;
+//             if (count % 10 == 0)
+//             {
+//                 printf("\n");
+//             }
+//         }
+//     }
+// }
+
+void is_prime(int n) // 前n个质数
 {
     int count = 0;
-    for (int i = 2; i <= n; i++)
+    for (int i = 2; count < n + 1; i++)
     {
         int flag = 1;
         for (int j = 2; j * j <= i; j++)
@@ -31,7 +57,7 @@ int main()
     int n;
     printf("请输入n的值(n>=2):");
     scanf("%d", &n);
-    printf("%d以内的质数有:\n", n);
+    printf("前%d个质数:\n", n);
     is_prime(n);
     return 0;
 }
