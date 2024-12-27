18636c0d5db7832f65393306046a0841c6ef80a3
2403246954@qq.com <2403246954@qq.com>
2024-12-27 20:48:20 +0800

    新增

--- 12.27/test2.c
+++ 12.27/test2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char const *argv[])
 {
@@ -8,5 +9,13 @@ int main(int argc, char const *argv[])
     printf("%s\n", s1);
     printf("%s\n", s2);
     printf("%s\n", s3);
+    printf("%ld,%lu\n", strlen(s2), sizeof(s2));
+    printf("%ld,%lu\n", strlen(s3), sizeof(s3));
+    s3 = "bye";
+    printf("%s\n", s3);
+    s3 = "hello";
+    printf("%s\n", s3);
+    // s2 = "bye"这是错误的; s2数组不可变(创建时指向谁就始终指向谁，内容可变)
+    // s3 指针指向可以变，但指向的内容不可变(即不可修改指向的值),因为"hello"是字符串常量
     return 0;
 }
