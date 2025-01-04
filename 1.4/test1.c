5f42aadec604d271f367550578bfc8f298dffec0
2403246954@qq.com <2403246954@qq.com>
2025-01-04 15:13:10 +0800

    新增

--- 1.4/test1.c
+++ 1.4/test1.c
@@ -9,9 +9,20 @@ struct MyStruct
     char b;   // 占1字节，不改变对齐
 };
 
+void print(struct MyStruct *p)
+{
+    printf("%d\n", p->a);
+    printf("%f\n", p->c);
+    printf("%d\n", p->b);
+}
+
 int main()
 {
     // 使用sizeof运算符来获取结构体的大小
+    struct MyStruct u1 = {4, 8, 6};
+    struct MyStruct u2 = u1; // 可以使用另一个对象对对象初始化
     printf("Size of MyStruct: %zu bytes\n", sizeof(struct MyStruct));
+    print(&u1);
+    print(&u2);
     return 0;
 }
