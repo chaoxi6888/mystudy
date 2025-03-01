569ce05814ca03fa1024e2972999beaba8a7bdc0
2403246954@qq.com <2403246954@qq.com>
2025-03-01 13:15:13 +0800

    新增短网址转换（简）

--- homework/2025/1.4/短网址转换.c
+++ homework/2025/1.4/短网址转换.c
@@ -8,18 +8,44 @@ const char BASE62[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTU
 
 struct url // 结构体
 {
-
-    char URL[100];
-    char shortURL[20];
+    unsigned int id;
+    char URL[100];     // 长链
+    char shortURL[20]; // 短链
 };
 
-unsigned long long create() // 随机生成id
+unsigned int generate_random_billion()
 {
-    // 使用当前时间初始化种子
-    srand(time(NULL));
-    // 生成随机数
-    int n = rand();
-    return n;
+    // 组合多个 rand() 调用来生成更大的随机数
+    unsigned int random_value = 0;
+    random_value = (rand() << 15) | rand(); // 将两个 rand() 的结果组合
+    return random_value % 1000000000;       // 限制在0到999,999,999之间
+}
+
+void create(struct url urls[], int l) // 随机生成不重复的id
+{
+    srand((unsigned)time(NULL)); // 设置随机数种子
+
+    for (int i = 0; i < l;)
+    {
+        int flag = 1;
+        unsigned int temp = generate_random_billion(); // 使用改进的随机数生成方法
+
+        // 检验是否有重复的
+        for (int j = 0; j < i; j++)
+        { // 只检查已生成的ID
+            if (temp == urls[j].id)
+            {
+                flag = 0;
+                break; // 如果发现重复，立即跳出循环
+            }
+        }
+
+        if (flag)
+        {
+            urls[i].id = temp;
+            i++; // 只有在生成了不重复的ID时才移动到下一个元素
+        }
+    }
 }
 
 void inverse(char *result, int l) // 翻转字符串
@@ -52,20 +78,33 @@ void encode_base62(unsigned long long num, char *result) // 根据id生成短码
     inverse(result, strlen(result));
 }
 
-void print(struct url ur) // 输出结构体的内容
+void print(struct url urls[], int l) // 输出结构体的内容
 {
-    printf("%s\n", ur.URL);
-    printf("%s\n", ur.shortURL);
+    printf("序号\tid\t\t长链\t\t\t\t\t短链\n");
+    for (int i = 0; i < l; i++)
+    {
+        printf("%d\t%u\t%.99s\t%.19s\n", i + 1, urls[i].id, urls[i].URL, urls[i].shortURL);
+    }
+}
+
+void encode(struct url urls[], int l) // 将每一个长链进行编码
+{
+    for (int i = 0; i < l; i++)
+    {
+        encode_base62(urls[i].id, urls[i].shortURL);
+    }
 }
 
 int main()
 {
-    struct url url1;
-    unsigned long long id;
-    strncpy(url1.URL, "8d608d0b-9a33-4831-8c8a-d2efa350c4fe", 100);
-    id = create();
-    printf("%lld\n", id);
-    encode_base62(id, url1.shortURL);
-    print(url1);
+    struct url urls[2];
+    int len = 2;
+    strncpy(urls[0].URL, "8d608d0b-9a33-4831-8c8a-d2efa350c4fe", sizeof(urls[0].URL) - 1);
+    strncpy(urls[1].URL, "22cad6f4-69cb-4a32-af1e-936360d29675", sizeof(urls[1].URL) - 1);
+    urls[0].URL[sizeof(urls[0].URL) - 1] = '\0'; // 确保以空字符结尾
+    urls[1].URL[sizeof(urls[1].URL) - 1] = '\0'; // 确保以空字符结尾
+    create(urls, len);
+    encode(urls, len);
+    print(urls, len);
     return 0;
 }
