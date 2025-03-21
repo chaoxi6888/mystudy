2df87fd36b198057b95c42abdcb3aae2d1ca8dbe
2403246954@qq.com <2403246954@qq.com>
2025-03-21 20:05:51 +0800

    完成部分队列的实现

--- homework/2025/3/14(双链表)/DoubleLlist.c
+++ homework/2025/3/14(双链表)/DoubleLlist.c
@@ -4,9 +4,9 @@
 // 定义双链表节点结构
 typedef struct Node
 {
-    int data;          // 数据域
-    struct Node *prev; // 指向前一个节点的指针
-    struct Node *next; // 指向后一个节点的指针
+    int data;           // 数据域
+    struct Node *prior; // 指向前一个节点的指针
+    struct Node *next;  // 指向后一个节点的指针
 } Node;
 
 // 创建新节点
@@ -19,69 +19,27 @@ Node *createNode(int data)
         exit(1);
     }
     newNode->data = data;
-    newNode->prev = NULL;
+    newNode->prior = NULL;
     newNode->next = NULL;
     return newNode;
 }
 
+// 初始化链表
+void init(Node *head, Node **end)
+{
+    head->prior = NULL;
+    head->next = NULL;
+    head->data = -1;
+    *end = head;
+}
+
 // 在链表末尾插入节点
-void add(Node **head, int data)
+void add(Node **end, int data)
 {
-    Node *newNode = createNode(data);
-    if (*head == NULL)
-    {
-        *head = newNode;
-        return;
-    }
-    Node *temp = *head;
-    while (temp->next != NULL)
-    {
-        temp = temp->next;
-    }
-    temp->next = newNode;
-    newNode->prev = temp;
-}
-
-// 在链表开头插入节点
-void addbegin(Node **head, int data)
-{
-    Node *newNode = createNode(data);
-    newNode->next = *head;
-    if (*head != NULL)
-    {
-        (*head)->prev = newNode;
-    }
-    *head = newNode;
-}
-
-void insert() {}
-
-// 删除链表中的节点
-void deleteNode(Node **head, int data)
-{
-    Node *temp = *head;
-    while (temp != NULL && temp->data != data)
-    {
-        temp = temp->next;
-    }
-    if (temp == NULL)
-    {
-        printf("未找到要删除的节点\n");
-        return;
-    }
-    if (temp->prev != NULL)
-    {
-        temp->prev->next = temp->next;
-    }
-    else
-    {
-        *head = temp->next;
-    }
-    if (temp->next != NULL)
-    {
-        temp->next->prev = temp->prev;
-    }
-    free(temp);
+    Node *node = createNode(data);
+    (*end)->next = node;
+    node->prior = *end;
+    *end = node;
 }
 
 // 打印链表
@@ -110,25 +68,11 @@ void freeList(Node *head)
 
 int main()
 {
-    Node *head = NULL;
-
-    // 插入节点
-    add(&head, 10);
-    add(&head, 20);
-    add(&head, 30);
-    addbegin(&head, 5);
-
-    // 打印链表
-    printf("双链表: ");
+    Node *head = (Node *)malloc(sizeof(Node));
+    Node *end;
+    init(head, &end); // 传递 end 的地址
+    add(&end, 10);    // 传递 end 的地址
     show(head);
-
-    // 删除节点
-    deleteNode(&head, 10);
-    printf("删除节点后的链表: ");
-    show(head);
-
-    // 释放链表内存
     freeList(head);
-
     return 0;
 }
